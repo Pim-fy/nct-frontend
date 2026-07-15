838f18b2e8d215784e0a7e0f794d7e0c35f8d6d5
Pim-fy <climent5373@naver.com>
2026-07-15 14:08:15 +0900

    fix: 경로 이중 프리픽스 수정

--- nct-frontend/src/api/reviewApi.js
+++ nct-frontend/src/api/reviewApi.js
@@ -3,16 +3,16 @@ import api from './axios';
 
 /** 리뷰 목록 조회 */
 export const getReviews = (params) =>
-  api.get('/api/reviews', { params }).then(res => res.data);
+  api.get('/reviews', { params }).then(res => res.data);
 
 /** 리뷰 등록 */
 export const createReview = (data) =>
-  api.post('/api/reviews', data).then(res => res.data);
+  api.post('/reviews', data).then(res => res.data);
 
 /** 리뷰 수정 */
 export const updateReview = (id, data) =>
-  api.put(`/api/reviews/${id}`, data).then(res => res.data);
+  api.put(`/reviews/${id}`, data).then(res => res.data);
 
 /** 리뷰 삭제 */
 export const deleteReview = (id) =>
-  api.delete(`/api/reviews/${id}`).then(res => res.data);
+  api.delete(`/reviews/${id}`).then(res => res.data);
